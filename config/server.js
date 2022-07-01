52be5863b53b9bc9679729c6bb54d8ac76661109
nqam <nghiemminh1904@gmail.com>
2022-07-01 20:41:42 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1bcb3a3d55084fcefb926252686d054b'),
+    },
+  },
+});
